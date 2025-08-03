e4d107e9771e835e3ab9c2be55e12f705d48154b
Dante <dante.tf5@gmail.com>
2025-08-03 14:50:41 -0300

    move simulation to ai-delver-intelligence

--- runtime/simulation/__init__.py
+++
@@ -1,4 +0,0 @@
-from .simulation import Simulation
-from .delver_action import DelverAction
-
-__all__ = ["Simulation", "DelverAction"]
--- runtime/simulation/delver_action.py
+++
@@ -1,6 +0,0 @@
-from typing import TypedDict
-
-
-class DelverAction(TypedDict):
-    move: bool
-    move_angle: float
